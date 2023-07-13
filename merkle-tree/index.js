ac0d0f7a4203b3398853702e67e57993ed80b23c
kphed <kpcrypto69@gmail.com>
2023-07-13 17:43:27 -0400

    Add logic for retrieving and storing merkle proofs

--- merkle-tree/index.js
+++ merkle-tree/index.js
@@ -10,5 +10,22 @@ const claimAddressesMatrix = claimAddresses.map((address) => [address]);
 // https://github.com/OpenZeppelin/merkle-tree#building-a-tree
 const tree = StandardMerkleTree.of(claimAddressesMatrix, ['address']);
 
+// Proofs keyed by addresses for easy look-up on the frontend
+const merkleProofsByAddress = {};
+
+// Iterate over entries and retrieve proofs for each one
+for (const [index, value] of tree.entries()) {
+  const [address] = value;
+
+  // Set proofs keyed by addresses for easy look-up by the connected user's account (frontend)
+  merkleProofsByAddress[address] = tree.getProof(index);
+}
+
 // Write the merkle root to a text file which will be stored on-chain for proof verification
 fs.writeFileSync(`${__dirname}/merkleRoot.txt`, tree.root);
+
+// Write the merkle proofs to a JSON file which will be utilized on the frontend
+fs.writeFileSync(
+  `${__dirname}/merkleProofs.json`,
+  JSON.stringify(merkleProofsByAddress)
+);
