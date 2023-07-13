d268ad786c82f881995e0a775e2ef679d59e9483
kphed <kpcrypto69@gmail.com>
2023-07-13 18:00:58 -0400

    Init ANFD claims contracts w/ forge

---
+++ src/AngryDogeClaims.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+/**
+ * @title  ANFD claims using a merkle tree-based system
+ * @author kphed (GitHub) / ppmoon69 (Twitter)
+ */
+contract AngryDogeClaims {}
