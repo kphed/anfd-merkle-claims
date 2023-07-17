2eef4aacba18129b03009069640ce0b728b3dea4
kphed <kpcrypto69@gmail.com>
2023-07-17 10:23:24 -0400

    Update root and proofs in test, fix merkle-tree/index.js

--- merkle-tree/index.js
+++ merkle-tree/index.js
@@ -1,34 +1,31 @@
 const fs = require('fs');
 const { StandardMerkleTree } = require('@openzeppelin/merkle-tree');
 const claimAddresses = require('./claim-addresses.json');
-const proofs = require('./merkleProofs.json');
 
-console.log(proofs['0x7eAf8FC3eF869381Fb07aD7191bf31805F76CE72'.toLowerCase()]);
+// Array of arrays containing merkle tree leaf constituents
+// In the case of the ANFD drop, we only need the address (all addresses receive the same amount)
+const claimAddressesMatrix = claimAddresses.map((address) => [address]);
 
-// // Array of arrays containing merkle tree leaf constituents
-// // In the case of the ANFD drop, we only need the address (all addresses receive the same amount)
-// const claimAddressesMatrix = claimAddresses.map((address) => [address]);
+// Build the merkle tree using the ANFD claim addresses
+// https://github.com/OpenZeppelin/merkle-tree#building-a-tree
+const tree = StandardMerkleTree.of(claimAddressesMatrix, ['address']);
 
-// // Build the merkle tree using the ANFD claim addresses
-// // https://github.com/OpenZeppelin/merkle-tree#building-a-tree
-// const tree = StandardMerkleTree.of(claimAddressesMatrix, ['address']);
+// Proofs keyed by addresses for easy look-up on the frontend
+const merkleProofsByAddress = {};
 
-// // Proofs keyed by addresses for easy look-up on the frontend
-// const merkleProofsByAddress = {};
+// Iterate over entries and retrieve proofs for each one
+for (const [index, value] of tree.entries()) {
+  const [address] = value;
 
-// // Iterate over entries and retrieve proofs for each one
-// for (const [index, value] of tree.entries()) {
-//   const [address] = value;
+  // Set proofs keyed by addresses for easy look-up by the connected user's account (frontend)
+  merkleProofsByAddress[address] = tree.getProof(index);
+}
 
-//   // Set proofs keyed by addresses for easy look-up by the connected user's account (frontend)
-//   merkleProofsByAddress[address] = tree.getProof(index);
-// }
+// Write the merkle root to a text file which will be stored on-chain for proof verification
+fs.writeFileSync(`${__dirname}/merkleRoot.txt`, tree.root);
 
-// // Write the merkle root to a text file which will be stored on-chain for proof verification
-// fs.writeFileSync(`${__dirname}/merkleRoot.txt`, tree.root);
-
-// // Write the merkle proofs to a JSON file which will be utilized on the frontend
-// fs.writeFileSync(
-//   `${__dirname}/merkleProofs.json`,
-//   JSON.stringify(merkleProofsByAddress)
-// );
+// Write the merkle proofs to a JSON file which will be utilized on the frontend
+fs.writeFileSync(
+  `${__dirname}/merkleProofs.json`,
+  JSON.stringify(merkleProofsByAddress)
+);
--- test/AngryDogeClaims.t.sol
+++ test/AngryDogeClaims.t.sol
@@ -10,9 +10,9 @@ contract AngryDogeClaimsTest is Test {
     ERC20 private constant ANFD =
         ERC20(0x4F14cDBd815B79E9624121f564f24685c6B1211b);
 
-    // Randomly-selected address from the ANFD claims address list for testing purposes
+    // First address from the ANFD claims address list for testing purposes
     address private constant CLAIMER =
-        0x0000000000007F150Bd6f54c40A34d7C3d5e9f56;
+        0x7eAf8FC3eF869381Fb07aD7191bf31805F76CE72;
 
     // An account which has ANFD which we can use to fund the claims contract w/ ANFD
     address private constant ANFD_TOKEN_SOURCE =
@@ -23,49 +23,55 @@ contract AngryDogeClaimsTest is Test {
     bytes32[] private claimerProof = [
         // Bytes32 type not inferred so we need to explicitly cast the values
         bytes32(
-            0x563af0d9d7ad292d314728bbe4b8a9a4a70b940cea2616786655c438754c6e93
+            0x1048b4b510efdf8028813af3b737cd5a79b7dc89d4705ebed8fb5937eb70fa10
         ),
         bytes32(
-            0xe73731697ca1635721f3a8c3d1197aea99481b3dac9318c91a33b7859a9a5817
+            0x587d8572933b30f0e1d2d379a60c71d8a291073e3a0ba496448fa82a3a724f72
         ),
         bytes32(
-            0x9c99e880aae3c8b3063241ea2533120c6e4dc12cd17227f8d0fe777554cbc5e4
+            0x18b87320ba55568d0e5811569104fb83df5f5c669f1429864e94f9248df2e6fa
         ),
         bytes32(
-            0x2d73adc098d6899834eb0e4040e078f7a04043c16b6eff9e220d12d63331087a
+            0x1d1b613bcbb30dc5c2b2d121773a156e11659749acf3ada121b9643beb0d4e47
         ),
         bytes32(
-            0xc0d08485b4c531d76928dc65bc4c90491b78e77ef5ff0123230a9c7b9b14ab5b
+            0xcbf6ec4d458bb9b878dade90404bc3b0861e53c7027dbf92484243a1f916165a
         ),
         bytes32(
-            0x1930601397cfd92122a33e1e369b4a747d3a5b68f8fc80d53e3355cf8006f687
+            0xce35519c0467f50c3a9c9ffd134311aaf7e8d18f7014afcbfd4bed6f6a6691ce
         ),
         bytes32(
-            0xe978ac5508871b198dc3b61f0ae544e34ea19cf4582e8b8790fecc1b4d013d24
+            0x07133204246a8d1194b122818cb9253ba5553938d9576e0e5be97e4d00e03e89
         ),
         bytes32(
-            0x11c3cc76c99055e05a50665f2d92cba889d4936ca95722b96586e884f684d719
+            0x0ca4cc436231eb04249c3d291b4fa29c58402b0792612040fb9d3f5fbbd7dfac
         ),
         bytes32(
-            0xf0ee50f6a3439170a6dc1149cf9aa07c6f505860f13a24aa347cf2b747454fcc
+            0xc755f87a4c3bca153d7a796711a44a3cc70299317c41e83104873b7875e0de2f
         ),
         bytes32(
-            0xd3a1a8a2efea851b8b0c84e837ce2eb690f7d34515da9349e4d6d50afd6ab388
+            0xd6201973cee40ca0473307b79e2e7a991e94e2f35baab64e0dac24cf94a15343
         ),
         bytes32(
-            0x22a381cf5de594b2823e262db625b2789737d9c20f10b2709c3a958d5146cca2
+            0x68b6ea2c31f7ffad37f146f6e3112faa604e411ffe4fc9dce8b673371528795b
         ),
         bytes32(
-            0x62a404eac1b991c0d0c8978e10623aea67454f7d9ce43a2983f0610f166adf16
+            0x3750a8e74cd3984ed51c6fa5fdbff864ee809d452eb96ba5941b469097273bfd
         ),
         bytes32(
-            0xb737b97a7389096b4086c38e25beddb2ba11333eda824854ecfcd6292cdd2421
+            0x4937d80247a8254e624f741adbd4ebe38073153d6c1c7c80a9e9dcb642ef3845
         ),
         bytes32(
-            0xc242bfeb976725e140da8d5ca5703829d7ce30059ae59dc1c64ce92d36826b74
+            0x4a10a0999061c0dc723e831f74b228e9bf7d1667e185388c26c8f3b5451a5b4e
         ),
         bytes32(
-            0x5bf234dc42fbb700eaf7687248b0d1e530d805bd6006bf4c031b6e1923305596
+            0xd8ab0e918bd7cb838c7b1d7d6b95ed8d6c815ddcf6a9c842b92774432cc6c4a9
+        ),
+        bytes32(
+            0xe74acaec80335fc8f0506dcab966394b334048b3e9e0f8a26036245a3f6a3039
+        ),
+        bytes32(
+            0xc787b83498596a75a40627d146d22f0353e717d004eee209cbd957eb3f21843d
         )
     ];
 
@@ -73,7 +79,7 @@ contract AngryDogeClaimsTest is Test {
 
     constructor() {
         claims = new AngryDogeClaims(
-            0x5254cfacfc11c5c033fb3c8f4f95e681f317bc33827c905459a1e2447e449323
+            0xba3bbb0c37381d1de79f9c7efbb117cda807fbf830e68fc4eacb228421cfa78b
         );
 
         // Impersonate the ANFD token source and transfer ANFD balance to the claims contract
