0ba9d8c06b501910d9b94d46c82100483229c700
kphed <kpcrypto69@gmail.com>
2023-07-17 10:22:15 -0400

    Use correct claim address list, and regenerate root and proofs*
    *NOTE: Proofs file size is too large, please generate and test locally

--- merkle-tree/index.js
+++ merkle-tree/index.js
@@ -1,31 +1,34 @@
 const fs = require('fs');
 const { StandardMerkleTree } = require('@openzeppelin/merkle-tree');
 const claimAddresses = require('./claim-addresses.json');
+const proofs = require('./merkleProofs.json');
 
-// Array of arrays containing merkle tree leaf constituents
-// In the case of the ANFD drop, we only need the address (all addresses receive the same amount)
-const claimAddressesMatrix = claimAddresses.map((address) => [address]);
+console.log(proofs['0x7eAf8FC3eF869381Fb07aD7191bf31805F76CE72'.toLowerCase()]);
 
-// Build the merkle tree using the ANFD claim addresses
-// https://github.com/OpenZeppelin/merkle-tree#building-a-tree
-const tree = StandardMerkleTree.of(claimAddressesMatrix, ['address']);
+// // Array of arrays containing merkle tree leaf constituents
+// // In the case of the ANFD drop, we only need the address (all addresses receive the same amount)
+// const claimAddressesMatrix = claimAddresses.map((address) => [address]);
 
-// Proofs keyed by addresses for easy look-up on the frontend
-const merkleProofsByAddress = {};
+// // Build the merkle tree using the ANFD claim addresses
+// // https://github.com/OpenZeppelin/merkle-tree#building-a-tree
+// const tree = StandardMerkleTree.of(claimAddressesMatrix, ['address']);
 
-// Iterate over entries and retrieve proofs for each one
-for (const [index, value] of tree.entries()) {
-  const [address] = value;
+// // Proofs keyed by addresses for easy look-up on the frontend
+// const merkleProofsByAddress = {};
 
-  // Set proofs keyed by addresses for easy look-up by the connected user's account (frontend)
-  merkleProofsByAddress[address] = tree.getProof(index);
-}
+// // Iterate over entries and retrieve proofs for each one
+// for (const [index, value] of tree.entries()) {
+//   const [address] = value;
 
-// Write the merkle root to a text file which will be stored on-chain for proof verification
-fs.writeFileSync(`${__dirname}/merkleRoot.txt`, tree.root);
+//   // Set proofs keyed by addresses for easy look-up by the connected user's account (frontend)
+//   merkleProofsByAddress[address] = tree.getProof(index);
+// }
 
-// Write the merkle proofs to a JSON file which will be utilized on the frontend
-fs.writeFileSync(
-  `${__dirname}/merkleProofs.json`,
-  JSON.stringify(merkleProofsByAddress)
-);
+// // Write the merkle root to a text file which will be stored on-chain for proof verification
+// fs.writeFileSync(`${__dirname}/merkleRoot.txt`, tree.root);
+
+// // Write the merkle proofs to a JSON file which will be utilized on the frontend
+// fs.writeFileSync(
+//   `${__dirname}/merkleProofs.json`,
+//   JSON.stringify(merkleProofsByAddress)
+// );
