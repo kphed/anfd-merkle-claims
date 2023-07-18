7ccccbd025354d180eb90d88686d06310684a05b
kphed <kpcrypto69@gmail.com>
2023-07-18 07:53:56 -0400

    Ensure address is lowercase when looking up proof

--- merkle-tree/getProofForAddress.js
+++ merkle-tree/getProofForAddress.js
@@ -1,6 +1,6 @@
 const merkleProofs = require('./merkleProofs.json');
 
-const ADDRESS = '0xClaimerAddressGoesHere';
+const ADDRESS = '0x1d4B9b250B1Bd41DAA35d94BF9204Ec1b0494eE3';
 
 // Log the merkle proof for the address
-console.log(merkleProofs[ADDRESS]);
+console.log(merkleProofs[ADDRESS.toLowerCase()]);
